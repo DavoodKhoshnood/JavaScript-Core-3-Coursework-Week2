f6fc54ba2a82f6f93d587463a4910c51849511f7
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-03-02 04:25:31 +0000

    London 8 - Davood Khoshnood - JavaScript 3 - Week 2

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,9 +1,30 @@
 const contentDiv = document.getElementById('content')
-const btn = document.querySelector('button')
-const list = document.querySelector('ul')
+const btnReceive = document.createElement('button')
+const btnAppend = document.createElement('button')
+const imgDiv = document.createElement('div')
+const receivedImg = document.createElement('img')
+const list = document.createElement('ul')
+
+btnReceive.classList.add('btn', 'btn-success', 'btn-lg', 'm-3')
+btnAppend.classList.add('btn', 'btn-success', 'btn-lg')
+list.style.backgroundColor = '#f512d'
+imgDiv.style.backgroundColor = '#dcdbc0'
+imgDiv.style.padding = '10px'
+imgDiv.style.marginBottom = '10px'
+imgDiv.style.width = '100%'
+receivedImg.style.width = '300px'
+
+btnReceive.innerText = 'Receive'
+btnAppend.innerText = 'Append'
+
+contentDiv.append(btnReceive, btnAppend, imgDiv, list)
+imgDiv.appendChild(receivedImg)
+
 let photoApiUrl = `https://dog.ceo/api/breeds/image/random`
 
-btn.addEventListener('click', () => {
+///////// Receive button event ///////////
+btnReceive.addEventListener('click', (event) => {
+  event.preventDefault()
   try {
     getNewPhoto()
   } catch (err) {
@@ -11,23 +32,40 @@ btn.addEventListener('click', () => {
   }
 })
 
+///////// Append button event ///////////
+btnAppend.addEventListener('click', addPhotoToList)
+
 function getNewPhoto() {
   let promise = fetch(photoApiUrl)
   promise
     .then((res) => res.json())
-    .then((imgSrc) => addPhotoToList(imgSrc.message))
+    .then((imgSrc) => (receivedImg.src = imgSrc.message))
     .catch((err) => {
       alert(`Error : ${err.message}`)
     })
 }
 
-function addPhotoToList(imgSrc) {
+function addPhotoToList() {
   let newList = document.createElement('i')
   let newImg = document.createElement('img')
-  newImg.src = imgSrc
-  newImg.style.width = '400px'
 
-  newList.classList.add('list-group-item')
-  newList.appendChild(newImg)
+  newImg.src = receivedImg.src
+  newImg.style.width = '100px'
+
+  newList.classList.add('list-group-item', 'list-group-item-success')
+
   list.appendChild(newList)
+  newList.appendChild(newImg)
+
+  let btnDelete = document.createElement('button')
+  btnDelete.classList.add('btn', 'btn-danger', 'btn-small', 'float-right')
+  btnDelete.innerText = 'Delete'
+
+  newList.appendChild(btnDelete)
+
+  btnDelete.addEventListener('click', (event) => {
+    event.preventDefault()
+    let clickedBtn = event.target
+    clickedBtn.parentNode.remove()
+  })
 }
